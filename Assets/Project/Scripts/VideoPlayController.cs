991a1504ecfe2991f929daa276ce132606886f3d
橋本 啓 <akhashimoto@geechs.com>
2023-09-28 22:42:39 +0900

    動画読み込みボタン実装

---
+++ Assets/Project/Scripts/VideoPlayController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.EventSystems;
+
+public class VideoPlayController : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private EventTrigger loadTrigger;
+    
+
+    private void Awake()
+    {
+        Vector2 size = new Vector2(Screen.width, Screen.height);
+        (videoPlayer.transform as RectTransform).sizeDelta = size;
+        videoPlayer.gameObject.SetActive(false);
+
+        EventTrigger.Entry loadClick = new EventTrigger.Entry();
+        loadClick.eventID = EventTriggerType.PointerClick;
+        loadClick.callback.AddListener((data) => { PickVideo(); });
+        loadTrigger.triggers.Add(loadClick);
+    }
+
+    // 動画の読み込みと再生
+    private void PickVideo()
+    {
+        if (NativeGallery.IsMediaPickerBusy())
+            return;
+        
+        videoPlayer.gameObject.SetActive(true);
+        
+        NativeGallery.Permission permission = NativeGallery.GetVideoFromGallery((path) =>
+        {
+            if (path != null)
+            {
+                // 動画サイズを取得する
+                // NativeGallery.VideoProperties properties = NativeGallery.GetVideoProperties(path);
+                // Vector2 size = new Vector2(properties.width, properties.height);
+                // (videoPlayer.transform as RectTransform).sizeDelta = size;
+                // Debug.Log(size);
+                
+                // 動画の再生
+                videoPlayer.url = path;
+                videoPlayer.Play();
+            }
+        }, "Select a video" );
+    }
+}
